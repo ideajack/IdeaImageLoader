ed8fc19b58f9cc7f12b3be32abd2a54c7b4f09a7
idea_zxj <jack_eloise@163.com>
2017-11-19 21:42:54 +0800

    my target

--- app/src/main/java/com/jack/ideaimageloader/MainActivity.java
+++ app/src/main/java/com/jack/ideaimageloader/MainActivity.java
@@ -3,6 +3,10 @@ package com.jack.ideaimageloader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * target 1：实现imageloader的基本功能
+ *        2：做成library，提供对外方法
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
